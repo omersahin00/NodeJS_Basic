d8003049d8a628e715e11adf5940c885172ce05c
Ömer Şahin <omersahin0309@gmail.com>
2024-02-04 19:34:05 +0300

    Update

--- routes/user.js
+++ routes/user.js
@@ -6,41 +6,8 @@ const db = require("../data/db");
 
 const data = {
     title: "Popüler Kurslar",
-    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi"],
-    blogs: [
-        {
-            blogid: 1,
-            baslik: "Komple Uygulamalı Web Geliştirme",
-            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim: "1.jpeg",
-            anasayfa: true,
-            onay: true
-        },
-        {
-            blogid: 2,
-            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
-            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-            resim: "2.jpeg",
-            anasayfa: true,
-            onay: false
-        },
-        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false,
-            onay: false
-        },
-        {
-            blogid: 4,
-            baslik: "Sıfırdan Uygulamalı React Geliştirme: Hooks, Redux & Firebase",
-            aciklama: "En popüler frontend kütüphanesi React'i baştan sona uygulamalı projelerle öğren. Hooks, Redux, Webpack, Firebase ve Fazlası.",
-            resim: "4.jpeg",
-            anasayfa: false,
-            onay: true
-        }
-    ]
+    //categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi"],
+    // blogs: Veri tabanından çekiliyor.
 }
 
 router.use("/blogs/:blogid", function(req, res){
@@ -48,20 +15,42 @@ router.use("/blogs/:blogid", function(req, res){
 });
 
 router.use("/blogs", function(req, res){
-    res.render("users/blogs", data);
+    db.execute("select * from Blog where onay = 1")
+        .then(result => {
+            let blogs = result[0];
+            db.execute("select * from Category")
+                .then(result2 => {
+                    let categories = result2[0];
+                    res.render("users/blogs", {
+                        title: "Popüler Kurslar",
+                        blogs: blogs,
+                        categories: categories,
+                        anasayfa: blogs.anasayfa,
+                        onay: blogs.onay
+                    });
+                })
+                .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 });
 
 router.use("/", function(req, res){
-    db.execute("select * from Blog where anasayfa = 1")
+    db.execute("select * from Blog where anasayfa = 1 AND onay = 1")
         .then(result => {
             let blogs = result[0];
-            res.render("users/index", {
-                title: blogs.title,
-                blogs: blogs,
-                categories: data.categories, // Veri tabanında olmadığı için geçiçi olarak diziden çekildi.
-                anasayfa: blogs.anasayfa,
-                onay: blogs.onay
-            });
+            db.execute("select * from Category")
+                .then(result2 => {
+                    let categories = result2[0];
+                    console.log(categories);
+                    res.render("users/index", {
+                        title: "Popüler Kurslar", 
+                        blogs: blogs,
+                        categories: categories,
+                        anasayfa: blogs.anasayfa,
+                        onay: blogs.onay
+                    });
+                })
+                .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
 });
