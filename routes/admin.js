3317472ef59cf85a553ecce23bad0c217c4e075e
Ömer Şahin <omersahin0309@gmail.com>
2024-02-06 22:25:23 +0300

    Update

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/db");
 
-router.use("/blogs/create", function(req, res){
-    res.render("admin/blog-create");
+router.use("/blogs/create", async function(req, res){
+    try {
+        const [categories] = await db.execute("select * from Category");
+        res.render("admin/blog-create", {
+            title: "Add Blog",
+            categories: categories
+        });    
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 
 router.use("/blogs/:id", function(req, res){
